e3f4a62658af39fb45e0ca67b9c7055d2d7c6ba1
Thomas Grapperon <35562418+tgrapperon@users.noreply.github.com>
2021-08-08 23:08:59 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
   ],
   dependencies: [
     .package(name: "Benchmark", url: "https://github.com/google/swift-benchmark", from: "0.1.0"),
-    .package(url: "https://github.com/pointfreeco/combine-schedulers", .branch("fix-xcode13")),
+    .package(url: "https://github.com/tgrapperon/combine-schedulers", .branch("fix-xcode13")),
     .package(url: "https://github.com/pointfreeco/swift-case-paths", from: "0.4.0"),
     .package(url: "https://github.com/pointfreeco/swift-identified-collections", from: "0.1.0"),
     .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "0.1.0"),

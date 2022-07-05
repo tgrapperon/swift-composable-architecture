2c8638fd5bc722ab6ccc65ac462f5af9e321354a
Thomas Grapperon <35562418+tgrapperon@users.noreply.github.com>
2022-07-05 19:04:52 -0300

    Update Synchronicity.swift

--- Sources/ComposableArchitecture/Effects/Synchronicity.swift
+++ Sources/ComposableArchitecture/Effects/Synchronicity.swift
@@ -1,11 +1,11 @@
 extension Reducer {
   /// A reducer that re-evaluates synchronously individual `Effect(value:)` it generates.
-  public func syncValues() -> Self {
+  public func syncFeedback() -> Self {
     .init { state, action, environment in
       var action: Action? = action
       
-      while let newAction = action {
-        let effect = self.run(&state, newAction, environment)
+      while let syncAction = action {
+        let effect = self.run(&state, syncAction, environment)
         guard case let .value(syncAction) = effect else {
           return effect
         }

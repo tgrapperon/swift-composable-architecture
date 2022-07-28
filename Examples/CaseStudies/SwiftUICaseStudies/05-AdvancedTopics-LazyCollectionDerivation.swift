870de0b98fcddc63352e8f5acef3abb25e84b921
Thomas Grapperon <35562418+tgrapperon@users.noreply.github.com>
2022-07-27 23:59:34 -0300

    Update 05-AdvancedTopics-LazyCollectionDerivation.swift

--- Examples/CaseStudies/SwiftUICaseStudies/05-AdvancedTopics-LazyCollectionDerivation.swift
+++ Examples/CaseStudies/SwiftUICaseStudies/05-AdvancedTopics-LazyCollectionDerivation.swift
@@ -53,7 +53,7 @@ enum LazyCollectionDerivationStudy {
     { `self`, id, item in
       item.color = self.color
     } updateSource: { `self`, id, item in
-      // This is not a good idea an this should be performed in a reducer,
+      // This is not a good idea, and this should be performed in a reducer,
       // otherwise an action not changing the color could overwrite the parent's
       // color.
       self.color = item.color

f28403f1366cb58cd3fe13870627b138c3f684a4
Thomas Grapperon <35562418+tgrapperon@users.noreply.github.com>
2022-12-18 20:25:48 -0300

    wip

--- Examples/CaseStudies/SwiftUICaseStudiesTests/02-Effects-LongLivingTests.swift
+++ Examples/CaseStudies/SwiftUICaseStudiesTests/02-Effects-LongLivingTests.swift
@@ -5,14 +5,63 @@ import XCTest
 
 @MainActor
 final class LongLivingEffectsTests: XCTestCase {
-  func testReducer() async {
+  
+//  func testReducer() async {
+//    let store = TestStore(
+//      initialState: LongLivingEffects.State(),
+//      reducer: LongLivingEffects()
+//    )
+//
+//    store.dependencies.notifications.screenshots.makeControllable()
+//
+//    let task = await store.send(.task)
+//
+//    // Simulate a screenshot being taken
+//    store.dependencies.notifications.screenshots.post()
+//
+//    await store.receive(.userDidTakeScreenshotNotification) {
+//      $0.screenshotCount = 1
+//    }
+//
+//    // Simulate screen going away
+//    await task.cancel()
+//
+//    // Simulate a screenshot being taken to show no effects are executed.
+//    store.dependencies.notifications.screenshots.post()
+//
+//  }
+  
+//  func testReducer2() async {
+//    await DependencyValues.withValue(\.notifications.screenshots, .controllable(\.screenshots)) {
+//      let store = TestStore(
+//        initialState: LongLivingEffects.State(),
+//        reducer: LongLivingEffects()
+//      )
+//
+//      let task = await store.send(.task)
+//
+//      // Simulate a screenshot being taken
+//      store.dependencies.notifications.screenshots.post()
+//
+//      await store.receive(.userDidTakeScreenshotNotification) {
+//        $0.screenshotCount = 1
+//      }
+//
+//      // Simulate screen going away
+//      await task.cancel()
+//
+//      // Simulate a screenshot being taken to show no effects are executed.
+//      store.dependencies.notifications.screenshots.post()
+//    }
+//  }
+  
+  
+  func testReducer3() async {
     let store = TestStore(
       initialState: LongLivingEffects.State(),
       reducer: LongLivingEffects().dependency(\.notifications.screenshots, .controllable(\.screenshots))
     )
     
-//    store.dependencies.notifications.screenshots.makeControllable()
-
     let task = await store.send(.task)
 
     // Simulate a screenshot being taken

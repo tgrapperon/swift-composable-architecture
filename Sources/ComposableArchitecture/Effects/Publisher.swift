becd34616f81f9a06cebd29c07a10262135b4bf7
Thomas Grapperon <35562418+tgrapperon@users.noreply.github.com>
2023-02-17 19:14:53 -0300

    wip

--- Sources/ComposableArchitecture/Effects/Publisher.swift
+++ Sources/ComposableArchitecture/Effects/Publisher.swift
@@ -76,7 +76,7 @@ extension EffectPublisher {
   /// > }
   /// > ```
   ///
-  /// - Note: The `values` async sequence only requests one element to the publisher. In a few
+  /// - Note: The `values` async sequence only demands one element to the publisher. In a few
   /// cases, like the NSObject's Key-Value Observation publisher `.publisher(for: options:)` for
   /// example, the sequence will only generate one element, and you may want to override the request
   /// to `.unlimited` to receive all the events you were receiving when using Combine. You can use

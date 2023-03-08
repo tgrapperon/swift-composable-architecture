b9320646d45c749de8c908a20813f1ba23de70b1
Thomas Grapperon <35562418+tgrapperon@users.noreply.github.com>
2023-03-08 17:21:08 -0300

    Add StateProxy

--- Sources/ComposableArchitecture/Reducer/Reducers/ReducerReader.swift
+++ Sources/ComposableArchitecture/Reducer/Reducers/ReducerReader.swift
@@ -1,24 +1,45 @@
+import Dependencies
+
 /// A reducer that builds a reducer from the current state and action.
 public struct ReducerReader<State, Action, Reader: ReducerProtocol>: ReducerProtocol
 where Reader.State == State, Reader.Action == Action {
   @usableFromInline
-  let reader: (State, Action) -> Reader
+  let reader: (StateProxy<State>, Action) -> Reader
 
   /// Initializes a reducer that builds a reducer from the current state and action.
   ///
   /// - Parameter reader: A reducer builder that has access to the current state and action.
   @inlinable
-  public init(@ReducerBuilder<State, Action> _ reader: @escaping (State, Action) -> Reader) {
+  public init(
+    @ReducerBuilder<StateProxy<State>, Action> _ reader: @escaping (StateProxy<State>, Action) ->
+      Reader
+  ) {
     self.init(internal: reader)
   }
 
   @usableFromInline
-  init(internal reader: @escaping (State, Action) -> Reader) {
+  init(internal reader: @escaping (StateProxy<State>, Action) -> Reader) {
     self.reader = reader
   }
 
   @inlinable
   public func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
-    self.reader(state, action).reduce(into: &state, action: action)
+    self.reader(StateProxy(state), action).reduce(into: &state, action: action)
+  }
+}
+
+@dynamicMemberLookup
+public struct StateProxy<State> {
+  @usableFromInline
+  init(_ state: State) {
+    self.value = state
+  }
+  public let value: State
+  @Dependency(\.self) var dependencies
+  public subscript<Value>(dynamicMember keyPath: KeyPath<State, Value>) -> Value {
+    self.value[keyPath: keyPath]
+  }
+  public subscript<Value>(dependency: KeyPath<DependencyValues, Value>) -> Value {
+    self.dependencies[keyPath: dependency]
   }
 }
